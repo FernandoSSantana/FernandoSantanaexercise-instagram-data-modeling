829ef1c3bb7da80bda07f61f3ab657857b4ae4f1
Fernando Santana <fernandosbcn@gmail.com>
2024-12-16 01:13:49 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -1,29 +1,52 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from datetime import date
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'users'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id=Column(Integer, primary_key=True)
+    username=Column(String,unique=True,nullable=False)
+    firstname=Column(String,nullable=False)
+    lastname=Column(String,nullable=False)
+    email=Column(String,unique=True)
+    posts = relationship ('posts', backref="users")
+    followers = relationship ('followers', backref="users")
+    comments= relationship ('coments', backref="users")
+
+class Post(Base):
+    __tablename__= "posts"
+    id=Column(Integer, primary_key=True)
+    title=Column(String(400))
+    user_id=Column(Integer, ForeignKey("users.id"))
+    media_type=relationship ('medias', backref="posts")
+    Comment=relationship ('comments', backref="posts")
+
+class Media(Base):
+    __tablename__= "medias"
+    id=Column(Integer, primary_key=True)
+    type=Column(Enum('imagen','video'))
+    url= Column(String(400))
+    post_id=Column(Integer,ForeignKey("posts.id"))
+
+class Comment(Base):
+    __tablename__="comments"
+    id=Column(Integer,primary_key=True)
+    comment_text=Column(String(250))
+    author_id=Column(Integer, ForeignKey("users.id"))
+    post_id=Column(Integer,ForeignKey("posts.id"))
+
+class Follower(Base):
+    __tablename__="followers"
+    id=Column(Integer, primary_key=True)
+    user_from_id=Column(Integer, ForeignKey("users.id"))
+    user_to_id=Column(Integer,ForeignKey("users.id"))
 
     def to_dict(self):
         return {}
@@ -35,3 +58,45 @@ try:
 except Exception as e:
     print("There was a problem genering the diagram")
     raise e
+
+
+
+
+# Fallower
+# -------
+# user_from_id integer FK >- User.ID
+# user_to_id integer FK >- User.ID
+
+# User
+# -----
+# ID PK integer
+# username string  unique
+# firstname string
+# lastname string
+# email string unique
+
+# Post
+# ----
+# ID PK integer
+# user_id integer FK >- User.ID
+
+# Media
+# ------
+# ID PK integer
+# type enum("imagen","video")
+# url string
+# post_id int FK >- Post.ID
+
+# Comment
+# -----
+# ID PK integer
+# comment_text string
+# author_id integer FK >- User.ID
+# post_id integer FK >- Post.ID
+
+# # Direct
+# # --
+# # ID_chat PK integer
+# # mensaje string
+# # sender_id integer
+# # addressee_id integer
